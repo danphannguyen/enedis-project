0c03839abe8a7b797c27d395874501efb5d7b42e
Dan PN <77532827+Derlvx@users.noreply.github.com>
2025-03-02 00:29:36 +0100

    fix: :rotating_light:
    fix Image from Next instead of img & fix translation never use

--- src/app/[locale]/content.tsx
+++ src/app/[locale]/content.tsx
@@ -10,10 +10,10 @@ export default function ContentView() {
     return (
         <div id="content-wrapper">
             <section className="p-8 rounded-lg">
-                <h1 id='content-section-title' className='text-2xl'>Évolution de la thermosensibilité en France</h1>
+                <h1 id='content-section-title' className='text-2xl'>{t('titleGraph')}</h1>
             </section>
             <aside className="p-8 flex items-start justify-center rounded-lg">
-                <h2 id='content-aside-title' className='text-2xl'>Filtres</h2>
+                <h2 id='content-aside-title' className='text-2xl'>{t('titleAside')}</h2>
             </aside>
             
         </div>
--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,10 +1,11 @@
+import Image from "next/image";
 
 
 export default function Footer() {
 
     return (
         <div id='footer' className='fixed bottom-0 right-0 left-0 w-full flex items-center justify-center py-4'>
-            <img id="footer-logo" src="/icon/logo-footer.svg" alt="" />
+            <Image id="footer-logo" src="/icon/logo-footer.svg" alt="" width={128} height={128}/>
         </div>
     );
 }
